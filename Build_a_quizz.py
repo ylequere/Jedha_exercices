97deebc9f7e092bc3c58b06b144abffa7da28166
ylequere <ylequere@gmail.com>
2022-09-24 20:08:59 +0200

    Updating with tuple instead of dict.

--- Build_a_quizz.py
+++ Build_a_quizz.py
@@ -10,12 +10,11 @@ def answer_a_question(question, answer, lives):
     good_answer = False
     while (not good_answer) and (lives > 0):
         user_answer = input(question)
-        # print("Your answer :", user_answer)
         
         if user_answer.lower() != user_answer:
             print('Your answer after .lower() function :', user_answer.lower())
             
-        if answer  == user_answer.lower():
+        if answer.lower()  == user_answer.lower():
             good_answer = True
             print(f'Congrats {answer} is the right answer !')
         else:
@@ -24,15 +23,15 @@ def answer_a_question(question, answer, lives):
             print(f'Sorry, you have {lives} chances left')
     return lives
         
-questions_answers= {"What is my birthday year ? " : "1971",
-                    "How many children do I have ? " : "2",
-                    "Who is the richest man in the world ? ": "elon musk"
-                    }
+questions_answers= (("What is my birthday year ? ", "1971"),
+                    ("How many children do I have ? ", "2"),
+                    ("Who is the richest man in the world ? ", "elon musk")
+                    )
 
 user_lives = 3
 print(f'Welcome to our quizz!\nYou have {user_lives} lives.')
 
-for i, qa in enumerate(questions_answers.items()):
+for i, qa in enumerate(questions_answers):
     print(f'\nQuestion {i+1}')
     user_lives = answer_a_question(qa[0], qa[1], user_lives)
     if user_lives == 0:
